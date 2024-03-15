0bc67b3a6a2399153a4dc35af38bd4238b86a366
Steve D <111876089+Stevil-Kanevil@users.noreply.github.com>
2024-03-15 15:48:43 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
-alert('Hello World..');
-
 let favouriteFood = 'Buffalo Chicken Wings... YUM!';
 
+let car = {
+    make: 'Ford',
+    model: 'Puma',
+    color: 'Red',
+    mileage: 1000
+};
+
+alert('Hello World..');
+
 document.write(favouriteFood);
+
+car['model'] = 'SEAT';
+
+document.write(
+    ` ${car.make} 
+    ${car.model} 
+    ${car.color} 
+    ${car.mileage}`
+    );
+
+let cars = [
+    {make: 'Ford', model: 'Fiesta', bhp: 50},
+    {make: 'Seat', model: 'Leon', bhp: 180},
+    {make: 'Citroen', model: '2CV', bhp: 10}
+];
+
+let carsVar = length(cars);
+
+document.write(cars[1]['make']);
+
+document.write(carsVar);
