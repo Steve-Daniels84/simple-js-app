e4c16cc9190131c5a8170a9def18f118adf5a697
Steve D <111876089+Stevil-Kanevil@users.noreply.github.com>
2024-03-15 16:14:44 +0000

    Update scripts.js
    Added pokemon data

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let pokemonList = [
+    {name: 'Charmander', height: 1, types: ['fire']},
+    {name: 'Bulbasaur', height: 2, types: ['grass', 'poison']},
+    {name: 'Charizard', height: 3, types: ['fire', 'flying']},
+    {name: 'Beedrill', height: 1, types: ['bug', 'poison']},
+
+];
+
