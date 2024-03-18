d4f3575c9b67aa6a900ca51f127294ee6db60733
Steve D <111876089+Stevil-Kanevil@users.noreply.github.com>
2024-03-18 14:03:53 +0000

    Styling the pokemon results into a more cohesive card

--- js/scripts.js
+++ js/scripts.js
@@ -20,9 +20,19 @@ let pokemonList = [
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 2)
     {
-        document.write (`<div class="grid-item"><img class="pokemonImage" src=" ../img/${pokemonList[i].name}.png" alt+"Image of the ${pokemonList[i].name} pokemon.">Name: ${pokemonList[i].name} <br> Height: ${pokemonList[i].height} mtrs<br><br>Wow thats big! </div>`);
+        document.write (`<div class="grid-item"><img class="pokemonImage" src=" ../img/${pokemonList[i].name}.png" alt+"Image of the ${pokemonList[i].name} pokemon."><div class="pokemon-data"> Name: ${pokemonList[i].name}<br>Height: ${pokemonList[i].height} mtrs</div><p class="pokemon-comment">Wow thats big!</p><div class="pokemon-types-container">`);
+        
+        for (let j = 0; j < pokemonList[i].types.length; j++) {
+            document.write (`<div class="pokemon-type ${pokemonList[i].types[j]}"> ${pokemonList[i].types[j]}</div>`); 
+        }
+        document.write ("</div></div>");
     } else {
-        document.write (`<div class="grid-item"><img class="pokemonImage" src=" ../img/${pokemonList[i].name}.png" alt+"Image of the ${pokemonList[i].name} pokemon.">Name: ${pokemonList[i].name} <br> Height: ${pokemonList[i].height} mtrs </div>`);
+        document.write (`<div class="grid-item"><img class="pokemonImage" src=" ../img/${pokemonList[i].name}.png" alt+"Image of the ${pokemonList[i].name} pokemon."><div class="pokemon-data">Name: ${pokemonList[i].name}<br>Height: ${pokemonList[i].height} mtrs </div><p class="pokemon-comment"></p><div class="pokemon-types-container">`);
+
+        for (let j = 0; j < pokemonList[i].types.length; j++) {
+            document.write (`<div class="pokemon-type ${pokemonList[i].types[j]}"> ${pokemonList[i].types[j]} </div>`);
+        }
+        document.write ("</div></div>");
     }
 }
 
