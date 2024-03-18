836f35a38c2101a053202466ab2310ce295fdaff
Steve D <111876089+Stevil-Kanevil@users.noreply.github.com>
2024-03-18 09:34:14 +0000

    added script to inject a pokemon card onto the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
 let pokemonList = [
-    {name: 'Charmander', height: 1, types: ['fire']},
-    {name: 'Bulbasaur', height: 2, types: ['grass', 'poison']},
-    {name: 'Charizard', height: 3, types: ['fire', 'flying']},
-    {name: 'Beedrill', height: 1, types: ['bug', 'poison']},
-
+    {name: 'charmander', 
+    height: 1, 
+    types: ['fire']
+    },
+    {name: 'bulbasaur', 
+    height: 2, 
+    types: ['grass', 'poison']
+    },
+    {name: 'charizard', 
+    height: 3, 
+    types: ['fire', 'flying']
+    },
+    {name: 'beedrill', 
+    height: 1, 
+    types: ['bug', 'poison']
+    }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 2)
+    {
+        document.write (`<div class="grid-item"><img class="pokemonImage" src=" ../img/${pokemonList[i].name}.png" alt+"Image of the ${pokemonList[i].name} pokemon.">Name: ${pokemonList[i].name} <br> Height: ${pokemonList[i].height} mtrs<br><br>Wow thats big! </div>`);
+    } else {
+        document.write (`<div class="grid-item"><img class="pokemonImage" src=" ../img/${pokemonList[i].name}.png" alt+"Image of the ${pokemonList[i].name} pokemon.">Name: ${pokemonList[i].name} <br> Height: ${pokemonList[i].height} mtrs </div>`);
+    }
+}
 
