05c26b5a55f8f493dcd272baa5caf7e218df270e
Steve D <111876089+Stevil-Kanevil@users.noreply.github.com>
2024-03-21 13:55:51 +0000

    Adding search functionality

--- js/scripts.js
+++ js/scripts.js
@@ -48,7 +48,15 @@ let pokemonRepository = (function () {
 
     return {
         add: function (pokemon) {
-            pokemonList.push(pokemon);
+
+            let inputType = typeof (pokemon);
+
+            if (inputType === "object" ) {
+                pokemonList.push(pokemon);
+            } else {
+                return console.log("You must enter correct pokemon data in object form!");
+            }
+            
         },
         getAll: function () {
             return pokemonList;
@@ -56,17 +64,18 @@ let pokemonRepository = (function () {
     };
 })()
 
-let element = document.getElementsByClassName("pokedex-grid-container")[0];
-
 //Get all pokemon and add a single card for each in the grid container
 pokemonRepository.getAll().forEach (function(pokemon){
 
-    //Build the indivdual Pokemon card
+    let element = document.getElementsByClassName("pokedex-grid-container")[0];
+
+    //Build the indivdual Pokemon cards
     let html = `<div class="pokedex-grid-item">
                 <img class="pokemonImage" src="../img/pokemonImages/${pokemon.name}.png" alt="Image of the ${pokemon.name} pokemon.">
-                <div class="pokemon-data">Name: ${pokemon.name}<br>Height: ${pokemon.height}</div>
+                <div class="pokemon-data">Name: ${pokemon.name}<br>Height: ${pokemon.height}<br>Category: ${pokemon.category}</div>
                 <div class="pokemon-types-container">`;
 
+    //Iterate through types to build tag for each type
     pokemon.types.forEach (function(type){
 
         html += `<div class="pokemon-type ${type.toLowerCase()}">${type}</div>`;
@@ -75,4 +84,34 @@ pokemonRepository.getAll().forEach (function(pokemon){
     html += '</div></div>';
                 
     element.innerHTML += html;
-})
+});
+
+//monitor search input for a value
+let searchBox = document.getElementById('sValue');
+let searchParam = '';
+
+searchBox.addEventListener('input', function() {
+    searchParam = searchBox.value;
+});
+
+//search for user entered search parameter
+function pokemonSearch () {
+    
+    if (searchParam === '') {
+        //error message if no search paramater defined
+        let element = document.getElementsByClassName("pokedex-grid-container")[0];
+        
+        //change styling of current grid to allow error message to show
+        element.classList.add('search-error');
+
+        element.innerHTML = '<p class="search-error">You do not appear to have entered any search criteria, please add something and try again</p>';
+
+    } else {
+        //search data for name
+        console.log(searchParam);
+        //if no data exists remove all cards and show message
+
+        //if it does exist show the card in the middle of the screen
+    }
+};
+
